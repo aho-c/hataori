daf0a70fe08d04af7374ec72767a2b34bbbc1e08
Fukasawa Takashi <115371535+aho-c@users.noreply.github.com>
2022-12-21 14:47:18 +0900

    Delete hataori.bat

--- chrome_host/hataori.bat
+++
@@ -1,8 +0,0 @@
-rem hataori host
-rem (c) Fukasawa Takashi
-rem MIT License
-
-@echo off
-set python_path=py
-set active_path=%CD%
-%python_path% -u "%active_path%\host.py"2>>"%active_path%\err.log"
