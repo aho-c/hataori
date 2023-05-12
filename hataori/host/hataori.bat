1745180be79838cafdf5fd82949105a1d0e87906
Fukasawa <115371535+nokokon@users.noreply.github.com>
2023-05-12 13:28:47 +0900

    Delete hataori directory

--- hataori/host/hataori.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rem hataori host
-rem (c) Fukasawa Takashi
-rem MIT License
-
-set python_path=py
-set active_path=%CD%
-%python_path% -u "%active_path%\host.py"2>>"%active_path%\err.log"
