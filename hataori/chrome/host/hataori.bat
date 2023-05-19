1abe477d9541217346be0da04864a21f86f7316c
Fukasawa Takashi <115371535+nokokon@users.noreply.github.com>
2023-05-19 16:19:55 +0900

    Delete hataori/chrome directory

--- hataori/chrome/host/hataori.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rem hataori host
-rem License: MIT License (http://www.opensource.org/licenses/mit-license.php)
-rem  (c) 2023 Fukasawa Takashi
-
-set python_path=py
-set active_path=%CD%
-%python_path% -u "%active_path%\host.py"2>>"%active_path%\err.log"
